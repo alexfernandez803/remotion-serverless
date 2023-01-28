54eb3f96cb8f30bc173027023b56d1662f2ee0ec
Alex Fernandez <7582277+alexfernandez803@users.noreply.github.com>
2023-01-28 11:23:33 +1100

    add render config in serverless app for render process execution

--- serverless-app/handler.ts
+++ serverless-app/handler.ts
@@ -1,15 +1,51 @@
+import { getFunctions, renderMediaOnLambda } from "@remotion/lambda/client";
 import { Handler } from "aws-lambda";
+import { COMP_NAME, SITE_ID } from "./config";
+import { getRandomRegion } from "./utils/regions";
 
 export const render: Handler = async (event: any) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v0.1! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+  try {
+    const region = getRandomRegion();
+    const [first] = await getFunctions({
+      compatibleOnly: true,
+      region,
+    });
+    const { renderId, bucketName } = await renderMediaOnLambda({
+      region: region,
+      functionName: first.functionName,
+      serveUrl: SITE_ID,
+      composition: COMP_NAME,
+      inputProps: {},
+      codec: "h264",
+      imageFormat: "jpeg",
+      maxRetries: 1,
+      privacy: "private",
+    });
+
+    console.log(`RenderId=${renderId}, bucketName=${bucketName}`);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: "Video sent for rendering.",
+          renderId,
+          bucketName,
+        },
+        null,
+        2
+      ),
+    };
+  } catch (err) {
+    console.log("Error", err);
+    return {
+      statusCode: 400,
+      body: JSON.stringify(
+        {
+          message: err,
+        },
+        null,
+        2
+      ),
+    };
+  }
 };
